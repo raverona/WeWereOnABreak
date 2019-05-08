8ca7f846ac7862275593191c0955887ef695527e
Rafael Marinaro Verona <rverona@uolinc.com>
2019-05-08 15:23:01 -0300

    Create a message template and capture exceptions so they don't break the script
    I forgot to include the template on the previous commit

---
+++ reddit_comment_template.py
@@ -0,0 +1,7 @@
+from string import Template
+
+reddit_comment_template = Template("""\
+[$message]($gif_link)
+___
+^(I'm a bot, feedback is appreciated at [my subreddit](https://www.reddit.com/r/WeWereOnABreakBot/))
+""")
